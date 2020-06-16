f4395def3420f50f8d16b0e28f737d97d199de28
Nikolai Pribitkov <pribitkov2013@mail.ru>
2020-06-16 19:28:45 +0300

    Updates

---
+++ precache-manifest.3c970cd26c2199f73214e26d92f844d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eb5f1c79d9f558ad88a2f4d5eb41c47",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "0283d72d96958c9b5517",
+    "url": "/Todo/static/css/main.302c7b5d.chunk.css"
+  },
+  {
+    "revision": "7778b7404b52ab46bf38",
+    "url": "/Todo/static/js/2.1d7b1102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo/static/js/2.1d7b1102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0283d72d96958c9b5517",
+    "url": "/Todo/static/js/main.6e428789.chunk.js"
+  },
+  {
+    "revision": "3e3b6737d1e394a62eb0",
+    "url": "/Todo/static/js/runtime-main.c090f2cf.js"
+  }
+]);
